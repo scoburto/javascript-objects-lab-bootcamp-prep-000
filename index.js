bec546b683dccf7a7088c647ab6fb92a65cda53f
scoburto@gmail.com <scoburto@gmail.com>
2018-03-06 15:04:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function updateObjectWithKeyAndValue(object,key,value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
-  
+  object = Object.assign(key,value)
 }
 
 function deleteFromObjectByKey(object,key) {
