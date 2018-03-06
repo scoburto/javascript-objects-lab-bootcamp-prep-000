2bca59cdfb50b8d196b4d544b2ec32b2379b1797
scoburto@gmail.com <scoburto@gmail.com>
2018-03-06 15:06:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var recipes = new Object({});
 
 function updateObjectWithKeyAndValue(object,key,value) {
-  
+  var newItem = new Object({});
+  Object.assign(newItem,object,{[key]: value});
+  return newItem;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
-  object = Object.assign(key,value)
+  Object.assign(object,key,value);
+  return object;
 }
 
 function deleteFromObjectByKey(object,key) {
